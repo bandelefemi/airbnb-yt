841d7ab95f479b5d9c06228a820d4527167e6737
Femi Bandele <105954155+bandelefemi@users.noreply.github.com>
2022-10-22 23:51:05 +0100

    updated largeCard

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -15,7 +15,9 @@ const LargeCard = ({img, title, description, buttonText}) => {
          <div className='absolute top-32 left-12'>
             <h3 className='text-4xl mb-3 w-64'>{title}</h3>
             <p>{description}</p>
-            <button>{buttonText}</button>
+            <button className='text-sm p-2 rounded-lg hover:shadow-md 
+            active:scale-90 font-semibold transform duration-200 mt-5 
+            text-white bg-gray-900'>{buttonText}</button>
          </div>
     </section>
     
